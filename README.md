5d319fc9da70cea887fd5f976c49cfc9b573dfc0
The-Hayden <haydencpomare@gmail.com>
2022-09-24 14:21:27 +1200

    Working signup and login interface

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# NWEN304-GroupProject
-NWEN304 group project 
+to run:
+1. npm install
+2. npm start
+
+add '/login' to url to go to login page
+
+Current features:
+- Create user account
+- Password encryption and decryption
+- Sign up feature working 
+- Login feature working
+- Login and sign up interface
---
+++ js/index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+const users = [];
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const initialisePassport = require("./passport-config");
+initialisePassport(
+  passport,
+  (username) => users.find((user) => user.username === username),
+  (id) => users.find((user) => user.id === id)
+);
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Home page routes */
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views/index.html"));
+});
+
+/* Get all users */
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+/* Login page routes */
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views/login.html"));
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
+
+app.post("/signup", async (req, res) => {
+  try {
+    const pwrd = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      email: req.body.email,
+      username: req.body.username,
+      password: pwrd,
+    });
+    res.sendFile(path.join(__dirname, "..", "views/login.html"));
+  } catch (e) {
+    console.log(e);
+  }
+  console.log(users);
+});
+
+/* Add to above methods to check if user is logged in */
+
+function checkNotAuth(req, res, next) {
+    if (req.isAuthenticated()) {
+      res.sendFile(path.join(__dirname, "..", "views/index.html"));
+    }
+    next()
+  }
+
+app.listen(process.env.PORT || PORT, console.log(`Listening on port ${PORT}`));
