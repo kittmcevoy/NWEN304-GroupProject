a0a37f40fe9228179c43fb9dbfaf3c915a5475a6
kmcevoy <kitt.mcevoy@outlook.com>
2022-10-18 15:15:51 +1300

    set img to take type buffer instead of string

--- js/Item.js
+++ js/Item.js
@@ -5,12 +5,11 @@ const itemSchema = new mongoose.Schema(
     {
         title: {type: String, required:true, unique:true},
         desc:{type: String, required:true},
-        img: {type: String}, 
+        img: {type: Buffer, required:true},
         categories: {type: Array}, 
         size: {type: String}, 
         color: {type: String}, 
-        price: {type: Number, required:true}, 
-
+        price: {type: Number, required:true},
     },
     {timestamps: true}
 ); 
